004dd4ecc89abee75ee700f2da52f97df6ff446a
agarciag97 <agarciag@campusdigitalfp.com>
2025-01-22 10:55:24 +0100

    2

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -1,35 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class EnemyBehavior : MonoBehaviour
 {
-// 1
-public Transform PatrolRoute;
-// 2
-public List<Transform> Locations;
-void Start()
-{
-    // 3
-InitializePatrolRoute();
-}
-// 4
-void InitializePatrolRoute()
-{
-// 5
-foreach(Transform child in PatrolRoute) {
-// 6
-Locations.Add(child);
-}
-}
+    public Transform PatrolRoute;
+    public List<Transform> Locations;
+    private int _locationIndex = 0;
+    private NavMeshAgent _agent;
+    public Transform Player;
+    private GameBehavior _gameManager;
 
 
+    void Start()
+    {  
+        _agent = GetComponent<NavMeshAgent>();
+        Player = GameObject.Find("Player").transform;
+        InitializePatrolRoute();
+        MoveToNextPatrolLocation();
+        _gameManager = GameObject.Find("Game Manager").GetComponent<GameBehavior>();
+
+    }
+
+    void InitializePatrolRoute()
+    {
+        foreach(Transform child in PatrolRoute) {
+
+            Locations.Add(child);
+            }
+    }
+
+    void Update()
+    {
+        if(_agent.remainingDistance < 0.2f && !_agent.pathPending) {
+        MoveToNextPatrolLocation();
+    }
+}
+
+    void MoveToNextPatrolLocation()
+    {
+        if (Locations.Count == 0)
+            return;
+        _agent.destination = Locations[_locationIndex].position;
+        _locationIndex = (_locationIndex + 1) % Locations.Count;
+
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.name == "Player")
         {
             Debug.Log("Player detected - attack!");
+            _agent.destination = Player.position;
+            Debug.Log("Enemy detected!");
+
         }
     }
 
@@ -40,4 +66,13 @@ Locations.Add(child);
             Debug.Log("Player out of range, resume patrol");
         }
     }
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if(collision.gameObject.name == "Enemy")
+        {
+            _gameManager.HP -= 1;
+        }
+    }
+
 }
